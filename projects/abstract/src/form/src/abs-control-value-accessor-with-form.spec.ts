2560d681915c25e4d398cae87964102d2e0baee3
wanoo21 <ionprodan33@gmail.com>
2021-07-30 21:31:05 +0300

    test(form): control value Accessor tests

--- projects/abstract/src/form/src/abs-control-value-accessor-with-form.spec.ts
+++ projects/abstract/src/form/src/abs-control-value-accessor-with-form.spec.ts
@@ -1,7 +1,15 @@
-import { AbsControlValueAccessorWithForm } from './abs-control-value-accessor-with-form';
+import {FormGroup} from '@angular/forms';
+import {AbsControlValueAccessorWithForm} from './abs-control-value-accessor-with-form';
+
+class TestClass extends AbsControlValueAccessorWithForm<any> {
+  form = new FormGroup({});
+
+  writeValue(obj: any): void {
+  }
+}
 
 describe('AbsControlValueAccessorWithForm', () => {
   it('should create an instance', () => {
-    expect(new AbsControlValueAccessorWithForm()).toBeTruthy();
+    expect(new TestClass()).toBeTruthy();
   });
 });
