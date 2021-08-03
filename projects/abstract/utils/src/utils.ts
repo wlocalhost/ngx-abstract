606bf1c18fae95bb823d46e0283434717eb6a2bb
wanoo21 <ionprodan33@gmail.com>
2021-08-03 16:33:40 +0300

    feat(utils): getRootHost function

--- projects/abstract/utils/src/utils.ts
+++ projects/abstract/utils/src/utils.ts
@@ -11,3 +11,16 @@ export function randomString(from = 10, to = 5): string {
 export function isObjectEmpty(obj: object | null): boolean {
   return !Boolean(obj) || Object.keys(obj).length === 0 && obj.constructor === Object;
 }
+
+/**
+ * Get root host of current domain, if `hostname` is empty - `location.hostname` is used.
+ * @example
+ * ```ts
+ * getRootHost('api.my-domain.com') // will return my-domain.com
+ * ```
+ */
+export function getRootHost(hostname = location.hostname): string {
+  const root = hostname.split('.').reverse().splice(0, 2).reverse().join('.');
+  // localhost doesn't support (.) before it
+  return root === 'localhost' ? root : '.' + root;
+}
