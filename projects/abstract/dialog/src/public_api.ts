075306cb0ed69c8c372ca8905e43ec483e5178c5
wanoo21 <ionprodan33@gmail.com>
2021-11-06 13:50:38 +0200

    fix(dialog): Export NgxAbsDialogRef

--- projects/abstract/dialog/src/public_api.ts
+++ projects/abstract/dialog/src/public_api.ts
@@ -1,4 +1,5 @@
 export * from './ngx-abstract-dialog/ngx-abstract-dialog.module';
 export * from './ngx-abstract-dialog/ngx-abs-dialog-close.directive';
 export * from './ngx-abstract-dialog/ngx-abs-dialog.service';
+export * from './ngx-abstract-dialog/NgxAbsDialogRef';
 export * from './ngx-abstract-dialog/tokens';
